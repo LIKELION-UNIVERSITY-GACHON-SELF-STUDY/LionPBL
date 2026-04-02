7575787c94b3b7765f027b41a41b614684dde954
jeonghyorim <hyorimj@icloud.com>
2026-04-02 19:47:30 +0900

    week1 hyorim assignment

--- Main.java
+++ Main.java
@@ -7,34 +7,24 @@ public class Main{
     int n=sc.nextInt();
     sc.nextLine();
 
-    if(n<5){
+    while(n<5){
       System.out.println("[오류] 5 이상 입력해주세요.");
       System.out.println("저장할 아기사자 수를 5 이상 입력해주세요.");
       n=sc.nextInt();
       sc.nextLine();
-      System.out.println("아기사자 이름을 입력해주세요.");
-      String[] name =new String[n];
-      for(int i=0; i<n; i++){
-        name[i]=sc.nextLine();
-      }
-      System.out.println("아기사자 명단을 최종적으로 출력합니다.");
-      for(int i=0; i<n; i++){
-        System.out.println(i+1+"번: "+name[i]);
-      }
 
     }
-    else{
       System.out.println("아기사자 이름을 입력해주세요.");
       String[] name =new String[n];
-      for(int i=0; i< n; i++){
-        name[i]=sc.nextLine();
-      }
 
+      for(int i=0; i< n; i++) {
+        name[i] = sc.nextLine();
+      }
       System.out.println("아기사자 명단을 최종적으로 출력합니다.");
       for(int i=0; i<n; i++){
         System.out.println(i+1+"번: "+name[i]);
       }
     }
 
-  }
 }
+
